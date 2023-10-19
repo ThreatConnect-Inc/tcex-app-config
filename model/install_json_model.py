9e829dfe24f68c97e6ea7452a579e7459760f646
Bracey Summers <bsummers@threatconnect.com>
2023-10-18 20:00:04 -0500

    APP-4246 - [Playbook] Removed support for AOT execution

--- model/install_json_model.py
+++ model/install_json_model.py
@@ -868,10 +868,11 @@ class InstallJsonCommonModel(BaseModel):
         for feature in self.features:
             model = self.known_features.get(feature)
 
+            # exclude deprecated features
             if feature in deprecated_features:
-                # exclude deprecated features
                 continue
-            elif model is None:
+
+            if model is None:
                 # not sure what the feature is, but add it anyway
                 features.append(feature)
 
